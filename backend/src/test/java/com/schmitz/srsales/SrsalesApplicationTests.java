2a0c0ebd06a0697b864d34fdb1bde8d8b6944abf
Gustavo Schmitz <guschmitz@gmail.com>
2021-09-09 23:25:15 -0400

    Demo project with Spring and React

---
+++ backend/src/test/java/com/schmitz/srsales/SrsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schmitz.srsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
