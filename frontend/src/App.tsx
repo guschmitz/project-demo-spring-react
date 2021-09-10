94f9033e7ce9dbae52777d803d502bbaf9ebe375
Gustavo Schmitz <guschmitz@gmail.com>
2021-09-10 16:01:55 -0400

    Add static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import NavBar from "components/NavBar";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
+import BarChart from "components/BarChart";
 
 function App() {
   return (
     <>
     <NavBar />
     <div className="container">
-      <h1 className="text-primary">Hello World</h1>
+      <h1 className="text-primary py-3">Sales Dashboard</h1>
+      <div className="row px-3">
+        <div className="cal-sm-6">
+          <h5 className="text-center text-secondary">Success rate (%)</h5>
+          <BarChart />
+        </div>
+      </div>
+
+      <div className="py-3">
+        <h2 className="text-primary">All sales</h2>
+      </div>
       <DataTable />
     </div>
     <Footer />
