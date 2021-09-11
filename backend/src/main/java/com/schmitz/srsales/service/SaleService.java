98a01bc22bd7651d3a529734fd9c20ab40225cd3
Gustavo Schmitz <guschmitz@gmail.com>
2021-09-11 11:40:09 -0400

    Added group bt search

--- backend/src/main/java/com/schmitz/srsales/service/SaleService.java
+++ backend/src/main/java/com/schmitz/srsales/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.schmitz.srsales.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.schmitz.srsales.dto.SaleDTO;
+import com.schmitz.srsales.dto.SaleSuccessDTO;
+import com.schmitz.srsales.dto.SaleSumDTO;
 import com.schmitz.srsales.entities.Sale;
 import com.schmitz.srsales.repositories.SaleRepository;
 import com.schmitz.srsales.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,12 +11,12 @@ function App() {
     <div className="container">
       <h1 className="text-primary py-3">Sales Dashboard</h1>
       <div className="row px-3">
-        <div className="cal-sm-6">
+        <div className="col-sm-6">
           <h5 className="text-center text-secondary">Success rate (%)</h5>
           <BarChart />
         </div>
-        <div className="cal-sm-6">
-          <h5 className="text-center text-secondary">Success rate (%)</h5>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">All sales</h5>
           <DonutChart />
         </div>
       </div>
