5e360f738a01367a9ced452b815bf38f673bb94d
Gustavo Schmitz <guschmitz@gmail.com>
2021-09-11 14:44:54 -0400

    Added home and navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,15 +1,18 @@
 import ImgLogo from 'assets/img/logo.png'
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-        <div className="container">
-          <nav className="my-2 my-md-0 mr-md-3">
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3">
+          <Link to="/">
             <img src={ImgLogo} alt="Schmitz" width="120" />
-          </nav>
-        </div>
+          </Link>
+        </nav>
       </div>
-    );
-  }
-  
-  export default NavBar;
+    </div>
+  );
+}
+
+export default NavBar;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            
+        <>
+        <NavBar />
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">Spring & React</h1>
+                <p className="lead">Analyze your sales performance from different perspectives</p>
+                <hr />
+                <p>This application consists of displaying a dashboard from data provided by a back end built with Spring Boot.</p>
+                <Link className="btn btn-primary btn-lg" to="/dashboard">Dashboard</Link>
+            </div>
         </div>
+        <Footer />
+        </>
     );
-  }
-  
-  export default Home;
+}
+
+export default Home;
